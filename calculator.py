488e780d657a926bb08c8c8439d96ab9dbbb90af
“soyeonkim” <scatterfragrance@gmail.com>
2024-10-25 16:13:48 +0900

    Revert "Add square function"
    This reverts commit bf28203dfe6bd7a37525aa1ea9ab5fb593bcf20e.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,13 @@
 # 기본 계산기 !!
 
 def add(a, b):
-	return a+b
+		return a+b
 
 def subtract(a, b):
-	return a-b
+		return a-b
 
 def multiply(a, b):
-	return a * b
+		return a * b
 
 def divide_main(a, b):
     return a / b
-
-def square(a): 
-	return a * a
